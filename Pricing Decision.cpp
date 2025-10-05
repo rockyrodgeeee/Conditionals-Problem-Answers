353e58fa87e7309f8487fcc1775e079d3186696b
rockyrodgeeee <rodiame@gbox.adnu.edu.ph>
2025-10-05 23:29:46 +0800

    Pricing Decision.cpp

--- Pricing Decision.cpp
+++ Pricing Decision.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double moneyAmount = 0;
+    cout << "Enter your money: ";
+    cin >> moneyAmount;
+
+    if (moneyAmount >= 1.0) {
+        cout << "Enjoy your lemonade!" << endl;
+    } else {
+        cout << "Sorry, you need more money." << endl;
+    }
+
+    return 0;
+}
 
